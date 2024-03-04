8ffb11ff21bfa186076ef0d967614af9ca6c210e
mickeydarrenlau <darrenwjlau@gmail.com>
2024-03-04 10:03:44 +0800

    Version bumping

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,6 +1,6 @@
 import { readFileSync, writeFileSync } from "fs";
 
-const targetVersion = "1.2.1";
+const targetVersion = "1.2.2";
 
 // read minAppVersion from manifest.json and bump version to target version
 let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
